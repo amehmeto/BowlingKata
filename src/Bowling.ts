74fcffb3e1644023e9f667403cc029f411e07d55
Amehmeto <42amehmeto@gmail.com>
2020-03-13 18:53:33 +0100

    Managing spare score

--- src/Bowling.ts
+++ src/Bowling.ts
@@ -1,29 +1,38 @@
 export class Bowling {
     readonly MAX_FRAME_SCORE = 10
 
-    computeScore(scoreLine: string): number{
+    computeFinalScore(scoreLine: string): number{
         let score: number = 0
-        for (let i = 0 ; scoreLine[i] ; i++){
-            if (!isNaN(+scoreLine[i]))
-                score += +scoreLine[i]
-            if (scoreLine[i] === '/')
-                score = this.computeSpare(scoreLine, i, score)
-            if (scoreLine[i] === 'X')
-                score += this.MAX_FRAME_SCORE
-        }
+        for (let i = 0 ; scoreLine[i] ; i++)
+            score = this.incrementWithRollScore(scoreLine, i, score)
+        return score
+    }
+
+    private incrementWithRollScore(scoreLine: string, i: number, score: number) {
+        if (Number.isInteger(+scoreLine[i]))
+            score += +scoreLine[i]
+        if (scoreLine[i] === '/')
+            score = this.computeSpare(scoreLine, i, score)
+        if (scoreLine[i] === 'X')
+            score += this.MAX_FRAME_SCORE
         return score
     }
 
     private computeSpare(scoreLine: string, i: number, score: number) {
-        if (Number.isInteger(+scoreLine[i + 1])) {
-            score += this.maximizeScoreToTen(scoreLine, i)
-            score += +scoreLine[i + 1]
-        } else
-            score += this.maximizeScoreToTen(scoreLine, i)
+        score += this.maximizeScoreToTen(scoreLine, i)
+        score += this.nextRollScore(scoreLine, i)
         return score
     }
 
+    private nextRollScore(scoreLine: string, i: number) {
+        if (Number.isInteger(+scoreLine[i + 1]))
+            return +scoreLine[i + 1]
+        return 0
+    }
+
     private maximizeScoreToTen(scoreLine: string, i: number) {
-        return this.MAX_FRAME_SCORE - +scoreLine[i - 1]
+        if (Number.isInteger(+scoreLine[i - 1]))
+            return this.MAX_FRAME_SCORE - +scoreLine[i - 1]
+        return this.MAX_FRAME_SCORE
     }
 }
